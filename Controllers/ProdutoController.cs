0e6c6ca8c00747ed9e0fe5730477d2895960f8ee
ThiagoDuutra <ThiagoDuutra01@gmail.com>
2025-04-10 20:48:04 -0300

    feat: realizado crud basico produtos

--- Controllers/ProdutoController.cs
+++ Controllers/ProdutoController.cs
@@ -23,5 +23,64 @@ namespace ApiWeb.Controllers
             var produtos = _context.Produtos.ToList();
             return Ok(produtos);
         }
+
+        [HttpGet]
+        [Route("{pId}")]
+        public ActionResult<ProdutoModel> BuscarProdutoPorId(int pId)
+        {
+            var produto = _context.Produtos.Find(pId);
+            if (produto == null)
+                return NotFound("Registro não localizado!");
+
+            return Ok(produto);
+        }
+
+        [HttpPost]
+        public ActionResult<ProdutoModel> CriarProduto(ProdutoModel pProdutoModel)
+        {
+            if (pProdutoModel == null)
+                return BadRequest("Ocorreu um erro na solicitação!");
+
+            _context.Produtos.Add(pProdutoModel);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(BuscarProdutoPorId), new { id = pProdutoModel.Id }, pProdutoModel);
+        }
+
+
+        [HttpPut]
+        [Route("{pId}")]
+        public ActionResult<ProdutoModel> EditarProduto(ProdutoModel pProdutoModel, int pId)
+        {
+            var produto = _context.Produtos.Find(pId);
+            if (produto == null)
+                return NotFound("Produto não encontrado!");
+
+            produto.Nome = pProdutoModel.Nome;
+            produto.Descricao = pProdutoModel.Descricao;
+            produto.CodigoDeBarras = pProdutoModel.CodigoDeBarras;
+            produto.QuantidadeEstoque = pProdutoModel.QuantidadeEstoque;
+            produto.Marca = pProdutoModel.Marca;
+
+            _context.Produtos.Update(produto);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{pId}")]
+        public ActionResult<ProdutoModel> DeletarProduto(ProdutoModel pProdutoModel, int pId)
+        {
+            var produto = _context.Produtos.Find(pId);
+            if (produto == null)
+                return NotFound("Produto não encontrado!");
+
+            _context.Produtos.Remove(produto);
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
--- obj/Debug/net9.0/ApiWeb.AssemblyInfo.cs
+++ obj/Debug/net9.0/ApiWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67fdceea97375599976e9dbed45b28d0cc2d6a8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54cc915e6e2892ae2a02a44a3eadb647df315ae3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
